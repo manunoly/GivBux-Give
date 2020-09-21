18975e11a6e75126cdc0b2338e200bb69ecbb660
Javier Silva <wolfblazer155@gmail.com>
2020-09-21 17:49:42 -0400

    -Refactor- UPDATE environment DEV

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://apd.gvbx.io/ap/',
+  apiUrl: 'https://apd.gvbx.io/api/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://apd.gvbx.io/ap/',
+  apiUrl: 'https://apd.gvbx.io/api/',
 };
 
 /*
